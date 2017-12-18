b068556d8f0d19935b0bf4735a99d61130c72abd
Timothy Ng <ncy.timothy@gmail.com>
2017-12-17 20:33:52 -0800

    First commit

---
+++ Swipe-to-Delete/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swipe-to-Delete
+//
+//  Created by Timothy Ng on 12/17/17.
+//  Copyright © 2017 Timothy Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
